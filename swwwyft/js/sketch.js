e743df381629b1717f0816bde24fb0fbc8e4ad7a
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2019-10-30 02:37:03 +0000

    Removed excess code

--- swwwyft/js/sketch.js
+++ swwwyft/js/sketch.js
@@ -1,13 +1,3 @@
-$('a').on('click', function() {
-	var target = $(this).attr('rel');
-	$('#' + target).show().siblings('div').hide();
-});
-
-$('img', '#gifs-rows').hover(function() {
-	$('.preset-file').toggle();
-	$('.gif-file').toggle();
-});
-
 function openNav() {
 	document.getElementById('mySidenav').style.width = '350px';
 	document.getElementById('allContOverlay').style.display = 'block';
