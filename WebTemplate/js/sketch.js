3dd67d387bc6f870c41219613b5045c4497dd397
ThomasKerslake <33869524+ThomasKerslake@users.noreply.github.com>
2019-12-30 01:56:09 +0000

    Swwwyft web template

---
+++ WebTemplate/js/sketch.js
@@ -0,0 +1,17 @@
+var myLinks, myAbout, myExp, myEdu;
+function getLinks() {
+	myLinks = document.getElementById('links');
+	myLinks.scrollIntoView();
+}
+function getAbout() {
+	myAbout = document.getElementById('about');
+	myAbout.scrollIntoView();
+}
+function getExp() {
+	myExp = document.getElementById('exp');
+	myExp.scrollIntoView();
+}
+function getEdu() {
+	myEdu = document.getElementById('edu');
+	myEdu.scrollIntoView();
+}
